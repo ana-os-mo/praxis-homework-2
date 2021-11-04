5ce6a9bc0f1c1985630631c1bc0f1a8ac696ef9b
ana-os-mo <anmosoriomo@unal.edu.co>
2021-11-04 09:58:24 -0500

    git-go-node-vue

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,43 +1,32 @@
 #!/usr/bin/env bash
 
-# verifica si esta instalado
-# si lo esta no lo instala
 if ! command -v git &> /dev/null
 then
     sudo yum install -y git
 fi
-git --version
+echo "git -->" && git --version
 
-# ----------------------------------------------
-
-# descarga, instala y agrega al path
-if [[ ! -f go1.17.2.linux-amd64.tar.gz ]]
+if ! command -v go &> /dev/null
 then
     wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
-    sudo sh -c "echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile"
-    source /etc/profile
-else
     sudo rm -rf /usr/local/go
     sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
     sudo sh -c "echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile"
     source /etc/profile
 fi
-go version
+echo "go -->" && go version
 
-# ----------------------------------------------
-
-if [[ ! -f node-v16.13.0-linux-x64.tar.gz ]]
+if ! command -v node &> /dev/null
 then
-    wget -c https://nodejs.org/download/release/latest-gallium/node-v16.13.0-linux-x64.tar.gz
-    sudo tar -C /bin -xzf node-v16.13.0-linux-x64.tar.gz
-    sudo sh -c "echo export PATH=$PATH:/bin/node-v16.13.0-linux-x64/bin >> /etc/profile"
-    source /etc/profile
-else
-    sudo rm -rf /bin/node-v16.13.0-linux-x64
-    sudo tar -C /bin -xzf node-v16.13.0-linux-x64.tar.gz
-    sudo sh -c "echo export PATH=$PATH:/bin/node-v16.13.0-linux-x64/bin >> /etc/profile"
-    source /etc/profile
+    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+    sudo yum install -y nodejs
+    sudo npm install -g npm@latest
 fi
-node --version
-npm --version
+echo "node -->" && node -v
+echo "npm -->" && npm -v
+
+if ! command -v vue &> /dev/null
+then
+    sudo npm install -g @vue/cli
+fi
+echo "vue -->" && vue --version
